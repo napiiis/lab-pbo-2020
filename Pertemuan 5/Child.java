f51cef951847b3fa6348fbe9f786716df5cf5865
napiiis <72500111+napiiis@users.noreply.github.com>
2021-01-01 18:24:51 +0700

    Kode Pertemuan 5

---
+++ Pertemuan 5/Child.java
@@ -0,0 +1,11 @@
+public class Child extends Parent {
+    int x;
+    
+    /* method constructor ini digunakan
+    untuk menciptakan suatu objek */
+    public Child() 
+    {
+        super();
+        x = 5;
+    }
+}
---
+++ Pertemuan 5/Manajer.java
@@ -0,0 +1,25 @@
+public class Manajer extends Pegawai {
+    private String departemen;
+    
+    /* method constructor ini digunakan
+    untuk menciptakan suatu objek dengan parameter*/
+    public Manajer(String nama, double gaji, String departemen)
+    {
+        super(nama, gaji);
+        this.departemen = departemen;
+    }
+    
+    /* method Mutator yang digunakan untuk mengubah
+    suatu nilai dari keadaan suatu objek tetapi memiliki parameter*/
+    public void setDepartemen(String departemen)
+    {
+        this.departemen = departemen;
+    }
+    
+    /* method Accessor ini yang digunakan untuk mengembalikan
+    nilai dari departemen*/
+    public String getDepartemen()
+    {
+        return departemen;
+    }
+}
